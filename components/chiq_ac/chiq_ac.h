846807a00b6b285e9d68a3ac383c9c2d070f7b01
Pavel-ADM-System <spa@admsystem.ru>
2026-06-03 11:10:08 +0300

    Update chiq_ac.h

--- components/chiq_ac/chiq_ac.h
+++ components/chiq_ac/chiq_ac.h
@@ -4,7 +4,8 @@
 #include <array>
 #include <cinttypes>
 #include <cmath>
-#include <set>
+#include <cstring>
+#include <vector>
 
 #include "esphome/components/binary_sensor/binary_sensor.h"
 #include "esphome/components/climate/climate.h"
@@ -41,18 +42,18 @@ class ChiqAirCon : public climate::Climate, public Component, public uart::UARTD
   void set_supported_swing_modes(climate::ClimateSwingModeMask modes) { this->supported_swing_modes_ = modes; }
   void set_supported_presets(climate::ClimatePresetMask presets) { this->supported_presets_ = presets; }
   void set_custom_fan_modes(std::initializer_list<const char *> modes) {
-    this->supported_custom_fan_modes_.clear();
-    for (auto *mode : modes)
-      this->supported_custom_fan_modes_.emplace(mode);
+    this->supported_custom_fan_modes_.assign(modes.begin(), modes.end());
+    this->set_supported_custom_fan_modes(this->supported_custom_fan_modes_);
   }
 
   void setup() override {
     this->mode = climate::CLIMATE_MODE_OFF;
     this->target_temperature = 24.0f;
     this->current_temperature = NAN;
-    this->fan_mode = climate::CLIMATE_FAN_AUTO;
+    this->set_fan_mode_(climate::CLIMATE_FAN_AUTO);
     this->swing_mode = climate::CLIMATE_SWING_OFF;
     this->preset = climate::CLIMATE_PRESET_NONE;
+    this->set_supported_custom_fan_modes(this->supported_custom_fan_modes_);
   }
 
   void loop() override {
@@ -67,7 +68,7 @@ class ChiqAirCon : public climate::Climate, public Component, public uart::UARTD
 
   climate::ClimateTraits traits() override {
     climate::ClimateTraits traits;
-    traits.set_supports_current_temperature(true);
+    traits.add_feature_flags(climate::CLIMATE_SUPPORTS_CURRENT_TEMPERATURE);
     traits.set_supported_modes(this->supported_modes_);
     traits.add_supported_mode(climate::CLIMATE_MODE_OFF);
     traits.add_supported_mode(climate::CLIMATE_MODE_HEAT_COOL);
@@ -85,7 +86,6 @@ class ChiqAirCon : public climate::Climate, public Component, public uart::UARTD
     traits.set_supported_presets(this->supported_presets_);
     traits.add_supported_preset(climate::CLIMATE_PRESET_NONE);
     traits.add_supported_preset(climate::CLIMATE_PRESET_SLEEP);
-    traits.set_supported_custom_fan_modes(this->supported_custom_fan_modes_);
     traits.set_visual_min_temperature(Constants::MIN_TEMPERATURE);
     traits.set_visual_max_temperature(Constants::MAX_TEMPERATURE);
     traits.set_visual_temperature_step(Constants::TEMPERATURE_STEP);
@@ -98,13 +98,13 @@ class ChiqAirCon : public climate::Climate, public Component, public uart::UARTD
     if (call.get_target_temperature().has_value())
       this->target_temperature = *call.get_target_temperature();
     if (call.get_fan_mode().has_value())
-      this->fan_mode = *call.get_fan_mode();
+      this->set_fan_mode_(*call.get_fan_mode());
     if (call.get_swing_mode().has_value())
       this->swing_mode = *call.get_swing_mode();
     if (call.get_preset().has_value())
       this->preset = *call.get_preset();
-    if (call.get_custom_fan_mode().has_value())
-      this->custom_fan_mode = *call.get_custom_fan_mode();
+    if (call.has_custom_fan_mode())
+      this->set_custom_fan_mode_(call.get_custom_fan_mode());
 
     this->clamp_frontend_state_();
     this->send_command_();
@@ -197,14 +197,14 @@ class ChiqAirCon : public climate::Climate, public Component, public uart::UARTD
         this->current_temperature_sensor_->publish_state(this->current_temperature);
     }
 
-    this->fan_mode = this->decode_fan_(payload[7]);
+    this->set_fan_mode_(this->decode_fan_(payload[7]));
     this->swing_mode = payload[4] == 0x1A ? climate::CLIMATE_SWING_HORIZONTAL : climate::CLIMATE_SWING_OFF;
     this->preset = (payload[6] & 0x01) ? climate::CLIMATE_PRESET_SLEEP : climate::CLIMATE_PRESET_NONE;
-    this->custom_fan_mode = "";
+    this->clear_custom_fan_mode_();
     if (payload[6] & 0x08)
-      this->custom_fan_mode = Constants::TURBO;
+      this->set_custom_fan_mode_(Constants::TURBO);
     else if (payload[6] & 0x04)
-      this->custom_fan_mode = Constants::QUIET;
+      this->set_custom_fan_mode_(Constants::QUIET);
 
     if (this->turbo_sensor_ != nullptr)
       this->turbo_sensor_->publish_state(payload[6] & 0x08);
@@ -232,7 +232,7 @@ class ChiqAirCon : public climate::Climate, public Component, public uart::UARTD
     payload[4] = this->encode_swing_(this->swing_mode);
     payload[5] = this->encode_mode_(this->mode);
     payload[6] = this->encode_special_();
-    payload[7] = this->encode_fan_(this->fan_mode);
+    payload[7] = this->encode_fan_(this->fan_mode.value_or(climate::CLIMATE_FAN_AUTO));
     payload[8] = this->encode_temperature_(this->target_temperature);
     payload[9] = std::isnan(this->current_temperature) ? 0x00 : this->encode_temperature_(this->current_temperature);
     payload[15] = static_cast<uint8_t>((millis() / 3600000UL) % 24);
@@ -339,11 +339,16 @@ class ChiqAirCon : public climate::Climate, public Component, public uart::UARTD
       special |= this->last_payload_[6] & 0x02;
     if (this->preset == climate::CLIMATE_PRESET_SLEEP)
       special |= 0x01;
-    if (this->custom_fan_mode == Constants::TURBO)
+    const auto custom_fan = this->get_custom_fan_mode();
+    if (this->string_ref_equals_(custom_fan, Constants::TURBO))
       special |= 0x08;
-    if (this->custom_fan_mode == Constants::QUIET)
+    if (this->string_ref_equals_(custom_fan, Constants::QUIET))
       special |= 0x04;
     return special;
+  }
+
+  static bool string_ref_equals_(const StringRef &value, const char *expected) {
+    return value.size() == strlen(expected) && strncmp(value.c_str(), expected, value.size()) == 0;
   }
 
   void clamp_frontend_state_() {
@@ -367,10 +372,10 @@ class ChiqAirCon : public climate::Climate, public Component, public uart::UARTD
   bool have_payload_{false};
   std::array<uint8_t, PAYLOAD_SIZE> last_payload_{};
 
-  climate::ClimateModeMask supported_modes_{0};
-  climate::ClimateSwingModeMask supported_swing_modes_{0};
-  climate::ClimatePresetMask supported_presets_{0};
-  std::set<std::string> supported_custom_fan_modes_{Constants::TURBO, Constants::QUIET};
+  climate::ClimateModeMask supported_modes_{};
+  climate::ClimateSwingModeMask supported_swing_modes_{};
+  climate::ClimatePresetMask supported_presets_{};
+  std::vector<const char *> supported_custom_fan_modes_{Constants::TURBO, Constants::QUIET};
 
   sensor::Sensor *current_temperature_sensor_{nullptr};
   binary_sensor::BinarySensor *turbo_sensor_{nullptr};
